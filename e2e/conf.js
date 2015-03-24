64e030672ce642f39915b6847554b97b452a564f
Chris Trotman <chris@trotman.io>
2015-03-25 02:22:19 +1100

    [e2e] Use the jasmine 2.0 framework and a mocha style reporter

--- e2e/conf.js
+++ e2e/conf.js
@@ -1,6 +1,16 @@
+'use strict';
+
 exports.config = {
   seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
   capabilities: {
     'browserName': 'phantomjs'
+  },
+  framework: 'jasmine2',
+  onPrepare: function() {
+    var SpecReporter = require('jasmine-spec-reporter');
+    jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: true}));
+  },
+  jasmineNodeOpts: {
+    print: function() {}
   }
 };
