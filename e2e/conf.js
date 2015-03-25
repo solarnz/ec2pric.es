05300b58009a287abd2e30747d43b052da2c0695
Chris Trotman <chris@trotman.io>
2015-03-25 18:05:01 +1100

    [e2e] Switch back to chrome for tests, as phantomjs is not reliable

--- e2e/conf.js
+++ e2e/conf.js
@@ -3,7 +3,7 @@
 exports.config = {
   seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
   capabilities: {
-    'browserName': 'phantomjs'
+    'browserName': 'chrome'
   },
   framework: 'jasmine2',
   onPrepare: function() {
