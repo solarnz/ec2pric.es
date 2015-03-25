5c3261ddfeddcd08257483ecdca89e7172f71b5f
Chris Trotman <chris@trotman.io>
2015-03-25 18:05:28 +1100

    [e2e] Implement some more comprehensive tests for region filtering

--- e2e/filters.spec.js
+++ e2e/filters.spec.js
@@ -1,6 +1,12 @@
 'use strict';
 
 describe('filtering', function() {
+  var INSTANCE_COUNT = 42;
+
+  function getInstanceList() {
+    return element.all(by.repeater('instance in instances'));
+  }
+
   beforeEach(function() {
     browser.get('http://localhost:3000/');
   });
@@ -9,26 +15,61 @@ describe('filtering', function() {
     var regionFilterDropdown;
 
     beforeEach(function() {
-      regionFilterDropdown = element.all(by.model('displayConfig.region'));
+      regionFilterDropdown = element(by.model('displayConfig.region'));
     });
 
     describe('Dropdown menu', function() {
       it('should exist', function() {
-        expect(regionFilterDropdown.count()).toBe(1);
+        expect(regionFilterDropdown.isPresent()).toBeTruthy();
+      });
+
+      it('should have a default value of us-east-1', function() {
+        var selectedOption = regionFilterDropdown.$('option:checked');
+        expect(selectedOption.getText()).toBe('us-east-1 (N. Virginia)');
+      });
+    });
+
+    it('should not filter by default', function() {
+      expect(getInstanceList().count()).toBe(INSTANCE_COUNT);
+    });
+
+    describe('when changing the region (to sa-east-1)', function() {
+      beforeEach(function() {
+        regionFilterDropdown.$('option[value="8"]').click();
+      });
+
+      it('should have less instance types than the default', function() {
+        expect(getInstanceList().count()).toBeLessThan(INSTANCE_COUNT);
+      });
+
+      it('should filter out instances not in the selected region', function() {
+        getInstanceList().each(function(element) {
+          var instanceName = element.$('[data-e2e-target="instanceName"]')
+          .getText();
+          instanceName.then(function(text) {
+            expect(text).not.toEqual('c4.large');
+          });
+        });
       });
     });
   });
 
   describe('Operating system filter', function() {
-    var operatingSystemFilterDropdown;
+    var dropdown;
+    var dropdownOptions;
 
     beforeEach(function() {
-      operatingSystemFilterDropdown = element.all(by.model('displayConfig.os'));
+      dropdown = element(by.model('displayConfig.os'));
+      dropdownOptions = element.all(by.model('displayConfig.os'));
     });
 
     describe('Dropdown menu', function() {
       it('should exist', function() {
-        expect(operatingSystemFilterDropdown.count()).toBe(1);
+        expect(dropdownOptions.count()).toBe(1);
+      });
+
+      it('should not filter by default', function() {
+        expect(getInstanceList().count()).toBe(INSTANCE_COUNT);
       });
     });
   });
