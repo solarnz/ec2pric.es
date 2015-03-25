b8aaf479bd65e5851c99258b6d0b125685182b2d
Chris Trotman <chris@trotman.io>
2015-03-25 22:43:31 +1100

    [e2e] Run the end to end tests in firefox under xvfb on travis

--- e2e/conf.js
+++ e2e/conf.js
@@ -1,12 +1,9 @@
 'use strict';
 
 exports.config = {
-  sauceUser: process.env.SAUCE_USERNAME,
-  sauceKey: process.env.SAUCE_ACCESS_KEY,
   seleniumServerJar: '../node_modules/gulp-protractor/node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
   capabilities: {
-    'browserName': 'chrome',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+    'browserName': 'firefox',
   },
   framework: 'jasmine2',
   onPrepare: function() {
