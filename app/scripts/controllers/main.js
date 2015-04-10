c8f7a79c4be4e65d30e9fb0527e8b12d941d36cf
Chris Trotman <chris@trotman.io>
2015-04-11 09:56:58 +1000

    [webpack] Make the main controller compatible

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,29 +1,40 @@
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('ec2pricesApp')
-.controller('MainController', ($filter, $scope, instancesFactory,
-  operatingSystems, regions) => {
+  var controller = function(angular) {
+    angular.module('ec2pricesApp')
+    .controller('MainController', ($filter, $scope, instancesFactory,
+      operatingSystems, regions) => {
 
-  $scope.regions = [
-    for (name of regions)
-    {name: name, humanName: $filter('regionName')(name)}
-  ];
+      $scope.regions = [
+        for (name of regions)
+        {name: name, humanName: $filter('regionName')(name)}
+      ];
 
-  $scope.operatingSystems = [
-    for (name of operatingSystems)
-    {name: name, humanName: $filter('operatingSystemName')(name)}
-  ];
+      $scope.operatingSystems = [
+        for (name of operatingSystems)
+        {name: name, humanName: $filter('operatingSystemName')(name)}
+      ];
 
-  instancesFactory.getInstances().then(function(instances) {
-    $scope.instances = instances;
-  });
+      instancesFactory.getInstances().then(function(instances) {
+        $scope.instances = instances;
+      });
 
-  $scope.displayConfig = {
-    instanceName: true,
-    storageSize: true,
-    storageTotalSize: true,
-    price: true,
-    region: 'us-east-1',
-    os: 'linux'
+      $scope.displayConfig = {
+        instanceName: true,
+        storageSize: true,
+        storageTotalSize: true,
+        price: true,
+        region: 'us-east-1',
+        os: 'linux'
+      };
+    });
   };
-});
+
+
+  if (typeof module !== 'undefined' && module && module.exports) {
+    module.exports = controller;
+  } else {
+    controller(angular);
+  }
+}());
