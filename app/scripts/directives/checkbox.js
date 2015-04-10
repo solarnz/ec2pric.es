17b67cd6d3df85330641697f37e1763ef9000ce0
Chris Trotman <chris@trotman.io>
2015-04-11 09:59:13 +1000

    [webpack] Made the checkbox directive compatible

--- app/scripts/directives/checkbox.js
+++ app/scripts/directives/checkbox.js
@@ -1,10 +1,21 @@
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('ec2pricesApp')
-  .directive('checkbox', () => {
-    return {
-      restrict: 'E',
-      scope: {checked: '='},
-      templateUrl: '/directives/checkbox.html',
-    };
-  });
+  var directive = function(angular) {
+    angular.module('ec2pricesApp')
+      .directive('checkbox', () => {
+        return {
+          restrict: 'E',
+          scope: {checked: '='},
+          templateUrl: '/directives/checkbox.html',
+        };
+      });
+  };
+
+
+  if (typeof module !== 'undefined' && module && module.exports) {
+    module.exports = directive;
+  } else {
+    directive(angular);
+  }
+}());
