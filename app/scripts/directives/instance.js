42cebaa23b008576be08175b66584d868c1b19d1
Chris Trotman <chris@trotman.io>
2015-04-11 10:01:08 +1000

    [webpack] Made the instance directive compatible

--- app/scripts/directives/instance.js
+++ app/scripts/directives/instance.js
@@ -1,13 +1,23 @@
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('ec2pricesApp')
-  .directive('instance', () => {
-    return {
-      restrict: 'A',
-      scope: {
-        instance: '=',
-        displayConfig: '=',
-      },
-      templateUrl: '/directives/instance.html'
-    };
-  });
+  var directive = function(angular) {
+    angular.module('ec2pricesApp')
+      .directive('instance', () => {
+        return {
+          restrict: 'A',
+          scope: {
+            instance: '=',
+            displayConfig: '=',
+          },
+          templateUrl: '/directives/instance.html'
+        };
+      });
+  };
+
+  if (typeof module !== 'undefined' && module && module.exports) {
+    module.exports = directive;
+  } else {
+    directive(angular);
+  }
+}());
