4c4115b5f7249618b9b505bfd10ce6c9acdbcbd6
Chris Trotman <chris@trotman.io>
2015-04-11 10:10:19 +1000

    [webpack] Made the operating system filter compatible

--- app/scripts/filters/operatingsystems.js
+++ app/scripts/filters/operatingsystems.js
@@ -1,19 +1,29 @@
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('ec2pricesApp')
-  .filter('operatingSystemName', () => {
-    return (input) => {
-      switch (input) {
-        case 'linux':
-          return 'Linux';
-        case 'mswin':
-          return 'Windows';
-        case 'mswinSQL':
-          return 'Windows SQL';
-        case 'mswinSQLWeb':
-          return 'Windows SQL Web';
-        default:
-          return input;
-      }
-    };
-  });
+  var filter = function(angular) {
+    angular.module('ec2pricesApp')
+      .filter('operatingSystemName', () => {
+        return (input) => {
+          switch (input) {
+            case 'linux':
+              return 'Linux';
+            case 'mswin':
+              return 'Windows';
+            case 'mswinSQL':
+              return 'Windows SQL';
+            case 'mswinSQLWeb':
+              return 'Windows SQL Web';
+            default:
+              return input;
+          }
+        };
+      });
+  };
+
+  if (typeof module !== 'undefined' && module && module.exports) {
+    module.exports = filter;
+  } else {
+    filter(angular);
+  }
+}());
