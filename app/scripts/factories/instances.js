c209c1cf92fba3481123ea7e1df9f520167d65ea
Chris Trotman <chris@trotman.io>
2015-04-11 10:05:13 +1000

    [webpack] Made the instance factory compatible

--- app/scripts/factories/instances.js
+++ app/scripts/factories/instances.js
@@ -1,21 +1,31 @@
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('ec2pricesApp')
-.factory('instancesFactory', ($http, $q) => {
-  let cachedData = null;
+  var factory = function(angular) {
+    angular.module('ec2pricesApp')
+    .factory('instancesFactory', ($http, $q) => {
+      let cachedData = null;
 
-  let getInstances = () => {
-    if (cachedData) {
-      return $q.when(cachedData);
-    }
+      let getInstances = () => {
+        if (cachedData) {
+          return $q.when(cachedData);
+        }
 
-    return $http.get('/instances.json').then((data) => {
-      cachedData = data.data;
-      return data.data;
+        return $http.get('/instances.json').then((data) => {
+          cachedData = data.data;
+          return data.data;
+        });
+      };
+
+      return {
+        getInstances: getInstances
+      };
     });
   };
 
-  return {
-    getInstances: getInstances
-  };
-});
+  if (typeof module !== 'undefined' && module && module.exports) {
+    module.exports = factory;
+  } else {
+    factory(angular);
+  }
+}());
